fa369d00fd6f1736de0f1ce06d08ff75f2b08b0b
kucukkanat <htolgasahin@gmail.com>
2023-04-23 04:45:59 +0200

    update baseUrl

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   srcDir: "src",
   appearance: false,
   cleanUrls: true,
-  
+  base: '/wedding/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
